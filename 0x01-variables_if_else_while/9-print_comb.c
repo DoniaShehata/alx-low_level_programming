f7e2eb45e00008c674466b4ca1b01a42495a0442
Donia Shehata <doniashehata98@gmail.com>
2023-06-16 16:10:30 -0700

    101-print_comb4.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -3,7 +3,7 @@
 /**
  * main - Entry point
  *
- * Description: print all single digit numbers
+ * Description: print 0, 1, - 9
  *
  * Return: Always 0 (Success)
 */
@@ -14,10 +14,17 @@ int main(void)
 
 	while (num <= 9)
 	{
-		printf("%i", num);
+		putchar(num + 48);
+
+		if (num != 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+
 		++num;
 	}
-	printf("\n");
+	putchar('\n');
 
 	return (0);
 }
